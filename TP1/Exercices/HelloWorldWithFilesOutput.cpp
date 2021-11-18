c037285164c60b6e05904a87f5025f85485cf3ba
sarahabb <49044714+sarahabb@users.noreply.github.com>
2021-11-18 09:06:49 +0100

    Add files via upload

---
+++ TP1/Exercices/HelloWorldWithFilesOutput.cpp
@@ -0,0 +1,39 @@
+# include <cstdlib>
+# include <sstream>
+# include <string>
+# include <fstream>
+# include <iostream>
+# include <iomanip>
+# include <mpi.h>
+
+int main( int nargs, char* argv[] )
+{
+	
+	MPI_Init( &nargs, &argv );
+	MPI_Comm globComm;
+	MPI_Comm_dup(MPI_COMM_WORLD, &globComm);
+	// On interroge le communicateur global pour connaître le nombre de processus
+	// qui ont été lancés par l'utilisateur :
+	int nbp;
+	MPI_Comm_size(globComm, &nbp);
+	// On interroge le communicateur global pour connaître l'identifiant qui
+	// m'a été attribué ( en tant que processus ). Cet identifiant est compris
+	// entre 0 et nbp-1 ( nbp étant le nombre de processus qui ont été lancés par
+	// l'utilisateur )
+	int rank;
+	MPI_Comm_rank(globComm, &rank);
+	// Création d'un fichier pour ma propre sortie en écriture :
+	std::stringstream fileName;
+	fileName << "Output" << std::setfill('0') << std::setw(5) << rank << ".txt";
+	std::ofstream output( fileName.str().c_str() );
+
+	output << "Bonjour, je suis la tâche n° " << rank << " sur " << nbp << " tâches." << std::endl;
+
+	output.close();
+	// A la fin du programme, on doit synchroniser une dernière fois tous les processus
+	// afin qu'aucun processus ne se termine pendant que d'autres processus continue à
+	// tourner. Si on oublie cet instruction, on aura une plantage assuré des processus
+	// qui ne seront pas encore terminés.
+	MPI_Finalize();
+	return EXIT_SUCCESS;
+}
